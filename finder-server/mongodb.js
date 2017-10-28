f1b88f93c7068b6ceb2fada315da50c2b1b04298
ordepdev <pedrotavares.dev@gmail.com>
2017-10-28 15:18:38 +0100

    Setup mongoose connection

---
+++ finder-server/mongodb.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/finder', {
+  useMongoClient: true
+}).catch(err => console.error(err));
+
