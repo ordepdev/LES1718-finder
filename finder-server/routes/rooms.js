e718a323bd943143a0607d04119a8d1fd580c30e
ordepdev <pedrotavares.dev@gmail.com>
2017-10-28 15:20:06 +0100

    Setup finder-server app
    * Two endpoints for fetch all rooms and by name;
    * Endpoint to check the server health

---
+++ finder-server/routes/rooms.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const Room = require('../models/room');
+
+module.exports = [
+  {
+    method: 'GET',
+    path: '/rooms',
+    handler(request, reply) {
+      Room.find({}, '', function(err, rooms) {
+        if (err) {
+          throw err;
+        }
+        reply(rooms);
+      });
+    }
+  },
+  {
+    method: 'GET',
+    path: '/rooms/{name}',
+    handler(request, reply) {
+      Room.find({'name': request.params.name}, '', function(err, room) {
+        if (err) {
+          throw err;
+        }
+        if (!room.length) {
+          return reply({
+            "message": "Room [{room}] was not found."
+              .replace("{room}", request.params.name)
+          }).code(404);  
+        }
+        reply(room).code(200);
+      });
+    }
+  } 
+];
+
---
+++ finder-server/server.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const Hapi = require('hapi');
+const port = process.env.PORT || 3000;
+const server = new Hapi.Server();
+const mongodb = require('./mongodb');
+const rooms= require('./routes/Rooms');
+
+server.connection({ port: port, host: 'localhost' });
+server.start((err) => {
+  if (err) {
+    throw err;
+  }
+});
+
+server.route(rooms);
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    reply('Hello, world!');
+  }
+});
+
+server.register({
+  register: require("hapi-and-healthy"),
+  options: {
+    path: "/health"
+  }
+});
+
