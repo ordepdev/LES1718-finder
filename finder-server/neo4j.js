7cc9f4c514650fa03d4ef16aec9771c29fcade3f
ordepdev <pedrotavares.dev@gmail.com>
2017-11-11 18:25:39 +0000

    Neo4j Config

--- finder-server/neo4j.js
+++ finder-server/neo4j.js
@@ -1,9 +1,10 @@
 "use strict";
 
 const neo4j = require('neo4j-driver').v1;
-
-const db = neo4j.driver("bolt://localhost", 
-  neo4j.auth.basic("neo4j", "neo4j")); 
+const url = process.env.GRAPHENEDB_BOLT_URL || "bolt://localhost";
+const user = process.env.GRAPHENEDB_BOLT_USER || "neo4j";
+const password = process.env.GRAPHENEDB_BOLT_PASSWORD || "neo4j";
+const db = neo4j.driver(url, neo4j.auth.basic(user, password)); 
 
 module.exports = {
   db
