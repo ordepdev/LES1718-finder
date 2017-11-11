40bd1f3fbbbb377a1e99e0c503f1a1ea21c356e2
ordepdev <pedrotavares.dev@gmail.com>
2017-11-11 20:13:12 +0000

    Set defaults

--- finder-server/server.js
+++ finder-server/server.js
@@ -2,12 +2,13 @@
 
 const Hapi = require('hapi');
 const port = process.env.PORT || 3000;
+const host = process.env.HOST || '0.0.0.0';
 const server = new Hapi.Server();
 const mongodb = require('./mongodb');
 const rooms = require('./routes/rooms');
 const navigation = require("./routes/navigation"); 
 
-server.connection({ port: port, host: 'localhost' });
+server.connection({ port: port, host: host });
 server.start();
 
 server.route(rooms);
