33416a1cf1ed44e56df6b5b1077dbe17e44b892c
martavasconcelos <martavasconcelos14@gmail.com>
2017-11-22 08:58:32 +0000

    history and favorites view and icons

--- finder-client/src/components/history/fav.js
+++ finder-client/src/components/history/fav.js
@@ -8,12 +8,12 @@ class Fav extends Component {
     return (
       <div>
       <h1 className="tittle">My favorites</h1>
-      <div id="historyList">
-      <h2 className="info"> from Classroom B021 to Classroom B012 </h2>
-      <h2 className="info"> from Classroom B341 to Classroom B011 </h2>
-      <h2 className="info"> from Classroom B221 to Classroom B023 </h2>
-      <h2 className="info"> from Classroom B218 to Classroom B037 </h2>
-      </div>
+      <ul id="historyList">
+      <li>  <h2 className="info"> from Classroom B021 to Classroom B012 <img className="icon" alt="remove fav" src="./fav.png"/> </h2> </li>
+      <li>  <h2 className="info"> from Classroom B341 to Classroom B011 <img className="icon" alt="remove fav" src="./fav.png"/> </h2> </li>
+      <li>  <h2 className="info"> from Classroom B221 to Classroom B023 <img className="icon" alt="remove fav" src="./fav.png"/> </h2> </li>
+      <li>  <h2 className="info"> from Classroom B218 to Classroom B037 <img className="icon" alt="remove fav" src="./fav.png"/> </h2> </li>
+      </ul>
       </div>
     );
   }
--- finder-client/src/components/history/history.js
+++ finder-client/src/components/history/history.js
@@ -8,11 +8,13 @@ class history extends Component {
     return (
       <div>
       <h1 className="tittle">Last searches</h1>
-      <div id="historyList">
-      <h2 className="info"> from Classroom B021 to Classroom B012 </h2> 
-      <h2 className="info"> from Classroom B341 to Classroom B011 </h2>
-      <h2 className="info"> from Classroom B221 to Classroom B023 </h2>
-      <h2 className="info"> from Classroom B218 to Classroom B037 </h2>
+      <div >
+      <ul id="historyList">
+      <li>  <h2 className="info"> from Classroom B021 to Classroom B012 <img className="icon" alt="redo" src="../redo.png"/> </h2> </li>
+      <li>  <h2 className="info"> from Classroom B341 to Classroom B011 <img className="icon" alt="redo" src="./redo.png"/> </h2> </li>
+      <li>  <h2 className="info"> from Classroom B221 to Classroom B023 <img className="icon" alt="redo" src="./redo.png"/> </h2> </li>
+      <li>  <h2 className="info"> from Classroom B218 to Classroom B037 <img className="icon" alt="redo" src="./redo.png"/> </h2> </li>
+      </ul>
       </div>
       </div>
     );
