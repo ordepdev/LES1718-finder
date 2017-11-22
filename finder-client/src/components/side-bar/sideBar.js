ffd383206c23f78f610c1fb11ca19ff7d5362781
martavasconcelos <martavasconcelos14@gmail.com>
2017-11-22 22:34:46 +0000

    menu design and logo added

--- finder-client/src/components/side-bar/sideBar.js
+++ finder-client/src/components/side-bar/sideBar.js
@@ -5,6 +5,7 @@ class sideBar extends Component {
   render () {
     return (
       <div className="sideBar">
+      <img className="logo" alt="logo" src="../logo.png"/>
         <Menu authentication={this.props.authentication} />
       </div>
     );
